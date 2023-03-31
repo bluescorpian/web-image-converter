a225c4a5c0839c9c100a11dc12d60699bba0f8bc
bluescorpian519 <dsharryh27@gmail.com>
2023-03-31 22:29:48 +0200

    add saveBlob to utility

--- utility.js
+++ utility.js
@@ -3,3 +3,10 @@ export function canvasToBlob(canvas, filetype, quality) {
     canvas.toBlob((blob) => res(blob), 'image/' + (filetype === 'jpg' ? 'jpeg' : filetype), quality);
   });
 }
+
+export function saveBlob(filename, blob) {
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
+}
