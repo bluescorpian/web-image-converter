6cb9200296f11f29aa7631d76a522a6c62cf988b
bluescorpian519 <dsharryh27@gmail.com>
2023-05-23 14:15:46 +0200

    fix index.html

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			input: [resolve(__dirname, 'pdf.html'), resolve(__dirname, 'image.html')],
+			input: [resolve(__dirname, 'index.html'), resolve(__dirname, 'pdf.html'), resolve(__dirname, 'image.html')],
 		},
 	},
 });
